7127af0a05808c9ec459e8f245bb8fbc4b19ef56
Fathahu <ateesfathahudheen@gmail.com>
2023-07-26 09:55:13 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 80,
+      width: 180,
+      color: Colors.indigoAccent,
+    );
+  }
+}
